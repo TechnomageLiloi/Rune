1825707b7875163ee32984f7bda5f3dccab37f09
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-10-19 16:19:42 +0300

    [0.0.6] Add atom show to Rune.Atoms.Show API method.

--- Engine/API/Atoms/Show/Method.php
+++ Engine/API/Atoms/Show/Method.php
@@ -4,13 +4,20 @@ namespace Liloi\Rune\API\Atoms\Show;
 
 use Liloi\API\Response;
 use Liloi\Rune\API\Method as SuperMethod;
+use Liloi\Rune\Domain\Atoms\Manager as MapsManager;
 
 class Method extends SuperMethod
 {
     public static function execute(): Response
     {
+        $URL = $_SERVER['REQUEST_URI'];
+        $keyAtom = MapsManager::URLtoATOM($URL);
+        $entity = MapsManager::load($keyAtom);
+
         $response = new Response();
-        $response->set('render', static::render(__DIR__ . '/Template.tpl'));
+        $response->set('render', static::render(__DIR__ . '/Template.tpl', [
+            'entity' => $entity
+        ]));
         return $response;
     }
 }
--- Engine/API/Atoms/Show/Template.tpl
+++ Engine/API/Atoms/Show/Template.tpl
@@ -1,4 +1,16 @@
 <link href="/Engine/API/Atoms/Show/Style.css" rel="stylesheet" />
 <div id="atoms-show" class="stylo">
+    <h1 class="wrap-title">
+        <?php echo $entity->getTitle(); ?>
+    </h1>
 
+    <hr/>
+
+    <?php echo $entity->parseProgram(); ?>
+
+    <hr/>
+
+    <div id="game-maps-collection">
+        <script></script>
+    </div>
 </div>
