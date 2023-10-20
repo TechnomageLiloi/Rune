d1b5f3f26ce35bb6e418cbf2e5f2f8c3d8807bb4
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-10-20 09:09:53 +0300

    [0.0.13] Secure atom data show.

--- Engine/API/Atoms/Show/Method.php
+++ Engine/API/Atoms/Show/Method.php
@@ -6,6 +6,7 @@ use Liloi\API\Response;
 use Liloi\Rune\API\Method as SuperMethod;
 use Liloi\Rune\Domain\Atoms\Manager as AtomsManager;
 use Liloi\Rune\Security;
+use Liloi\Rune\Exceptions\AccessException;
 
 class Method extends SuperMethod
 {
@@ -14,7 +15,15 @@ class Method extends SuperMethod
         $URL = $_SERVER['REQUEST_URI'];
         $keyAtom = AtomsManager::URLtoATOM($URL);
         $entity = AtomsManager::load($keyAtom);
-        $children = AtomsManager::loadFiles($keyAtom);
+
+        $isAdmin = self::accessGet();
+
+        if(!$entity->isPublished() && !$isAdmin)
+        {
+            throw new AccessException();
+        }
+
+        $children = AtomsManager::loadFiles($keyAtom, !$isAdmin);
 
         $response = new Response();
         $response->set('render', static::render(__DIR__ . '/Template.tpl', [
