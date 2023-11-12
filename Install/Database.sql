c1c97a6b155eaea6df53490f19176e686ed65757
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-11-13 01:25:49 +0200

    [0.4.0] Add _questions database table.

--- Install/Database.sql
+++ Install/Database.sql
@@ -13,6 +13,23 @@ create table rune_atoms
         primary key (key_atom)
 );
 
+-- EXAMS
+
+create table rune_questions
+(
+    key_question bigint unsigned auto_increment,
+    title varchar(250) not null,
+    status tinyint unsigned default 1 not null,
+    type tinyint unsigned not null,
+    program json not null,
+    theory text not null,
+    tags varchar(100) not null,
+    dt timestamp not null,
+    data json not null,
+    constraint artifacts_questions_pk
+        primary key (key_question)
+);
+
 -- TARDIS
 
 CREATE TABLE `rune_logs` (
