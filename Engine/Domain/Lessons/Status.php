eb3671cf8777d44c3f80cab0705a1da34bc3ce54
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-12-13 02:40:30 +0200

    [0.7.7] Rename Absenteeism to Failure.

--- Engine/Domain/Lessons/Status.php
+++ Engine/Domain/Lessons/Status.php
@@ -7,12 +7,12 @@ class Status
     public const TODO = 1;
     public const IN_HAND = 2;
     public const COMPLETE = 3;
-    public const ABSENTEEISM = 4;
+    public const FAILURE = 4;
 
     public static $list = [
         self::TODO => 'To Do',
         self::IN_HAND => 'In hand',
         self::COMPLETE => 'Complete',
-        self::ABSENTEEISM => 'Absenteeism'
+        self::FAILURE => 'Failure'
     ];
 }
