6e58340c6d35318532b5d69a95f39916a8aa42a2
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-03-24 11:53:57 +0200

    [0.0.3] Update database source.

--- Install/Database.sql
+++ Install/Database.sql
@@ -1,10 +1,46 @@
-create table rune_atoms
+CREATE TABLE `rune_questions` (
+  `key_question` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `type` tinyint(3) unsigned NOT NULL,
+  `program` json NOT NULL,
+  `theory` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tags` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dt` timestamp NOT NULL,
+  `data` json NOT NULL,
+  `power` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`key_question`)
+);
+
+create table rune_report
 (
-    key_atom timestamp not null,
-    type tinyint unsigned default 1 not null,
+    key_report timestamp not null,
+    key_question bigint unsigned not null,
+    result bool not null,
+    comment varchar(250) not null,
+    data json not null,
+    constraint rune_report_pk
+        primary key (key_report, key_question),
+    constraint rune_report_rune_questions_key_question_fk
+        foreign key (key_question) references rune_questions (key_question)
+            on update cascade on delete cascade
+);
+
+create table rune_suites
+(
+    key_suite varchar(500) not null,
     title varchar(100) not null,
     summary text not null,
-    data json not null,
-    constraint rune_atoms_pk
-        primary key (key_atom)
+    constraint rune_suites_pk
+        primary key (key_suite)
 );
+
+insert into rune_suites(key_suite, title, summary) VALUES ('rune', 'Root', '-');
+
+alter table rune_questions
+    add key_suite varbinary(500) default 'rune' not null;
+
+alter table rune_questions
+    add constraint rune_questions_rune_suites_key_suite_fk
+        foreign key (key_suite) references rune_suites (key_suite)
+            on update cascade on delete cascade;
