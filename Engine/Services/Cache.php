261beaa05108a17dc5755b8077420179842e6989
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-08-08 01:28:13 +0300

    [0.0.21] Disable cache.

--- Engine/Services/Cache.php
+++ Engine/Services/Cache.php
@@ -9,7 +9,7 @@ use Liloi\Rune\Security;
 
 class Cache
 {
-    private const ENABLED = true;
+    private const ENABLED = false;
     public const CACHE_DIR = ROOT_DIR . '/Cache';
 
     private static function keyToFilename(string $key, bool $block = false): string
