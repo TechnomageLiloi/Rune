afd8b3c05c0314987af15d57d108819b12506457
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-08-08 22:59:04 +0300

    [0.0.22] Add config database table (SQL).

--- Install/Database.sql
+++ Install/Database.sql
@@ -21,3 +21,11 @@ create table rune_logs
 	constraint rune_logs_pk
 		primary key (key_log)
 );
+
+create table rune_config
+(
+    key_config varchar(100) not null,
+    data json not null,
+    constraint rune_config_pk
+        primary key (key_config)
+);
