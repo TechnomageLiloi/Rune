f1bcc622ee55070b1e48831e2eabcac10fb4d5a0
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-08-08 23:20:50 +0300

    [0.0.24] Add restore of last plan position.

--- Engine/API/Wiki/Show/Method.php
+++ Engine/API/Wiki/Show/Method.php
@@ -6,6 +6,8 @@ use Liloi\API\Response;
 use Liloi\Rune\API\Method as SuperMethod;
 use Liloi\Judex\Assert;
 use Liloi\Rune\Domain\Atoms\Manager as AtomsManager;
+use Liloi\Rune\Domain\Config\Manager as ConfigManager;
+use Liloi\Rune\Domain\Config\Keys as ConfigKeys;
 
 class Method extends SuperMethod
 {
@@ -13,7 +15,17 @@ class Method extends SuperMethod
     {
         $accessAdmin = self::accessGet();
 
-        $RID = AtomsManager::URLtoATOM($_SERVER['REQUEST_URI']);
+        $URL = $_SERVER['REQUEST_URI'];
+
+        if($URL === '/')
+        {
+            $RID = ConfigManager::load(ConfigKeys::CURRENT_RID)->getString() ?? 'rune';
+        }
+        else
+        {
+            $RID = AtomsManager::URLtoATOM($URL);
+            ConfigManager::load(ConfigKeys::CURRENT_RID)->setString($RID)->save();
+        }
 
         $entity = AtomsManager::load($RID);
         $children = AtomsManager::loadFiles($RID);
--- Engine/Domain/Config/Keys.php
+++ Engine/Domain/Config/Keys.php
@@ -2,10 +2,7 @@
 
 namespace Liloi\Rune\Domain\Config;
 
-use Liloi\Rune\Domain\Manager as DomainManager;
-
 class Keys
 {
-    public const CURRENT_DATE = 'current-date';
-    public const CURRENT_POSITION = 'current-position';
+    public const CURRENT_RID = 'current-rid';
 }
