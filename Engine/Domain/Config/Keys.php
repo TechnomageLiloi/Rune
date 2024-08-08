941d2135c06b05b33259f7bc9f45523b0561cb21
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-08-08 23:00:24 +0300

    [0.0.23] Add config domain.

---
+++ Engine/Domain/Config/Keys.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Liloi\Rune\Domain\Config;
+
+use Liloi\Rune\Domain\Manager as DomainManager;
+
+class Keys
+{
+    public const CURRENT_DATE = 'current-date';
+    public const CURRENT_POSITION = 'current-position';
+}
