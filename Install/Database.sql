0e27d19dcc9cf7d56790a3dacbd9ebaaaa70fda5
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-09-16 09:52:19 +0300

    [0.0.9] Add config and logs tables.

--- Install/Database.sql
+++ Install/Database.sql
@@ -11,3 +11,21 @@ create table rune_atoms
 );
 
 INSERT INTO rune_atoms (key_atom, title, program, status, data, wiki) VALUES ('rune', 'Root', '// Program', DEFAULT, '{}', '// Wiki');
+
+create table rune_logs
+(
+	key_log bigint unsigned auto_increment,
+	ts timestamp not null,
+	tags varchar(1000) not null,
+	data json not null,
+	constraint rune_logs_pk
+		primary key (key_log)
+);
+
+create table rune_config
+(
+    key_config varchar(100) not null,
+    data json not null,
+    constraint rune_config_pk
+        primary key (key_config)
+);
