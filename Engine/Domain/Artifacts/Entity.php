b23520422a13e5f8c4a7f0e4043694d4971df043
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-09-17 18:44:52 +0300

    [0.2.1] Add artifact domain.

---
+++ Engine/Domain/Artifacts/Entity.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Liloi\Rune\Domain\Artifacts;
+
+use Liloi\Stylo\Parser;
+use Liloi\Tools\Entity as AbstractEntity;
+
+/**
+ * @method string getType()
+ * @method void setType(string $value)
+ *
+ * @method string getTitle()
+ * @method void setTitle(string $value)
+ *
+ * @method string getDescription()
+ * @method void setDescription(string $value)
+ *
+ * @method string getGlobal()
+ * @method void setGlobal(string $value)
+ *
+ * @method string getLocal()
+ * @method void setLocal(string $value)
+ *
+ * @method string getData()
+ * @method void setData(string $value)
+ */
+class Entity extends AbstractEntity
+{
+    public function getKey(): string
+    {
+        return $this->getField('key_artifact');
+    }
+
+    public function getRID(): string
+    {
+        return $this->getField('key_atom');
+    }
+
+    public function save(): void
+    {
+        Manager::save($this);
+    }
+
+    public function getTimestampBeauty(): string
+    {
+        return date('Y F j (D) - g:i a', strtotime($this->getKey()));
+    }
+}
---
+++ Engine/Domain/Artifacts/Types.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Liloi\Rune\Domain\Artifacts;
+
+class Types
+{
+    public const SIMPLE_FILE = 1;
+
+    public static $list = [
+        self::SIMPLE_FILE => 'Simple File',
+    ];
+}
