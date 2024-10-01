8060c0d12ebd29e0ca58c22b51a416c608fa7b4b
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-10-02 02:01:27 +0300

    [0.5.6] Add business domain.

---
+++ Modules/Business/Domain/Imperials/Collection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Liloi\Rune\Modules\Business\Domain\Imperials;
+
+use Liloi\Tools\Collection as AbstractCollection;
+
+/**
+ * @package Engine\Domain\Exercise
+ */
+class Collection extends AbstractCollection
+{
+}
