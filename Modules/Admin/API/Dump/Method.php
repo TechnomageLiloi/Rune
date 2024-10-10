de58ff3a4a09ec6e33e17e8cb93a96d81f6ea4b0
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-10-10 08:22:20 +0300

    [0.7.6] Add JSON dump result.

--- Modules/Admin/API/Dump/Method.php
+++ Modules/Admin/API/Dump/Method.php
@@ -4,11 +4,14 @@ namespace Liloi\Rune\Modules\Admin\API\Dump;
 
 use Liloi\API\Response;
 use Liloi\Rune\API\Method as SuperMethod;
+use Liloi\Rune\Domain\Artifacts\Manager as ArtifactsManager;
 
 class Method extends SuperMethod
 {
     public static function execute(): Response
     {
+        ArtifactsManager::dump(ROOT_DIR . '/Pool/Archive.atom');
+
         $response = new Response();
         $response->set('render', static::render(__DIR__ . '/Template.tpl'));
         return $response;
