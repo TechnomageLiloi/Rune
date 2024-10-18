52aa9d179f1014567fb2f4c64d77cd3a796b6f2a
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-10-18 12:40:43 +0300

    [0.8.8] Add defending degree status.

--- Modules/Degrees/Domain/Degrees/Statuses.php
+++ Modules/Degrees/Domain/Degrees/Statuses.php
@@ -6,9 +6,11 @@ class Statuses
 {
     public const NOT_DEFENDED = 1;
     public const DEFENDED = 2;
+    public const DEFENDING = 3;
 
     public static $list = [
         self::NOT_DEFENDED => 'Not defended degree',
+        self::DEFENDING => 'Defending now degree',
         self::DEFENDED => 'Defended degree',
     ];
 }
