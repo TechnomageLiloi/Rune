9dfe4b92a3e79f5d4390e132514c0b1e3e8fb272
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-10-22 17:08:58 +0300

    [0.9.17] Add map field.

--- Modules/Maps/API/Show/Map.js
+++ Modules/Maps/API/Show/Map.js
@@ -3,8 +3,21 @@ let Map = {
     {
         let map = document.getElementById("map");
         let context = map.getContext("2d");
-        context.moveTo(0, 0);
-        context.lineTo(400, 400);
+        context.font = "15px Arial";
+
+        context.fillStyle = "black";
+        context.fillRect(0,0,400,400);
+        let size = 15;
+
+        context.fillStyle = "white";
+        for(let y=-size;y<=size;y++)
+        {
+            for(let x=-size;x<=size;x++)
+            {
+                context.fillText('.', 10 * (x + size) + 50, 10 * (y + size) + 50);
+            }
+        }
+
         context.stroke();
     }
 };
