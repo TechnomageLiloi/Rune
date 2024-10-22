8f6ccf34b295d028d1287e983b9a31a69699929e
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-10-22 17:16:34 +0300

    [0.9.18] Add map field PC.

--- Modules/Maps/API/Show/Map.js
+++ Modules/Maps/API/Show/Map.js
@@ -1,9 +1,12 @@
 let Map = {
     start: function ()
     {
+        var PCx = 0;
+        var PCy = 0;
+
         let map = document.getElementById("map");
         let context = map.getContext("2d");
-        context.font = "15px Arial";
+        context.font = "15px monospace";
 
         context.fillStyle = "black";
         context.fillRect(0,0,400,400);
@@ -14,6 +17,12 @@ let Map = {
         {
             for(let x=-size;x<=size;x++)
             {
+                if(PCx === x && PCy ===y)
+                {
+                    context.fillText('@', 10 * (x + size) + 50, 10 * (y + size) + 50);
+                    continue;
+                }
+
                 context.fillText('.', 10 * (x + size) + 50, 10 * (y + size) + 50);
             }
         }
