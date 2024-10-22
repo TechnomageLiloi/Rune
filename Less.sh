7f0573e23f4ba4edfc526575170ad82163ea053f
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-10-22 16:28:24 +0300

    [0.9.13] Add import LESS to map.

--- Less.sh
+++ Less.sh
@@ -5,3 +5,4 @@ lessc ./Engine/API/Style.less ./Engine/API/Style.css
 lessc ./Modules/Diary/API/Road/Show/Style.less ./Modules/Diary/API/Road/Show/Style.css
 lessc ./Modules/Admin/API/Report/Style.less ./Modules/Admin/API/Report/Style.css
 lessc ./Modules/Admin/API/Dashboard/Style.less ./Modules/Admin/API/Dashboard/Style.css
+lessc ./Modules/Maps/API/Show/Style.less ./Modules/Maps/API/Show/Style.css
--- Modules/Maps/API/Show/Template.tpl
+++ Modules/Maps/API/Show/Template.tpl
@@ -1,3 +1,11 @@
-<div>
-    <h1>Map</h1>
+<link href="<?php echo ROOT_URL; ?>/Modules/Maps/API/Show/Style.css" rel="stylesheet" />
+
+<div id="modules-maps-show">
+    <table>
+        <tr>
+            <td>A</td>
+            <td>B</td>
+            <td>C</td>
+        </tr>
+    </table>
 </div>
