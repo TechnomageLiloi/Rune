7408ae3460bcf5f0ee8e74bd113886fa8b83df5f
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-12-03 02:37:29 +0200

    [0.04.007] Add Modules/Databank/Domain/Scrolls test.

---
+++ Modules/Databank/Domain/Scrolls/Manager.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Liloi\Rune\Modules\Databank\Domain\Scrolls;
+
+use Liloi\Rune\Domain\Manager as DomainManager;
+
+class Manager extends DomainManager
+{
+    /**
+     * Get table name.
+     *
+     * @return string
+     */
+    public static function getTableName(): string
+    {
+        return self::getTablePrefix() . 'scrolls';
+    }
+
+    public static function loadCollection(): Collection
+    {
+        $name = self::getTableName();
+
+        $rows = self::getAdapter()->getArray(sprintf(
+            'select * from %s order by key_scroll desc;',
+            $name
+        ));
+
+        $collection = new Collection();
+
+        foreach($rows as $row)
+        {
+            $collection[] = Entity::create($row);
+        }
+
+        return $collection;
+    }
+
+    public static function load(string $key): Entity
+    {
+        $name = self::getTableName();
+
+        $row = self::getAdapter()->getRow(sprintf(
+            'select * from %s where key_scroll="%s"',
+            $name,
+            $key
+        ));
+
+        return Entity::create($row);
+    }
+
+    public static function save(Entity $entity): void
+    {
+        $name = self::getTableName();
+        $data = $entity->get();
+
+        // @todo: Get param name from const.
+        $key = $data['key_scroll'];
+        unset($data['key_scroll']);
+
+        self::getAdapter()->update(
+            $name,
+            $data,
+            sprintf('key_scroll = "%s"', $key)
+        );
+    }
+
+    // @todo: rise this method to more abstract level.
+    public static function create(string $keyAtom): void
+    {
+        $name = self::getTableName();
+        self::getAdapter()->insert($name, [
+            'key_atom' => $keyAtom,
+            'title' => 'Enter the title',
+            'scroll' => 'Enter the scroll',
+        ]);
+    }
+}
---
+++ Modules/Databank/Domain/Scrolls/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Liloi\Rune\Modules\Databank\Domain\Scrolls;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Check phpUnit testing ability.
+ */
+class Test extends TestCase
+{
+    public function testCheckFileExistence(): void
+    {
+        $this->assertTrue(file_exists(__DIR__ . '/Collection.php'));
+        $this->assertTrue(file_exists(__DIR__ . '/Entity.php'));
+        $this->assertTrue(file_exists(__DIR__ . '/Manager.php'));
+    }
+}
