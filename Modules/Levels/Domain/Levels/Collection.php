65801508ca673183d294f9a40c7d0b2666d9d3e3
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-12-06 22:51:00 +0200

    [0.05.017] Add levels domain.

---
+++ Modules/Levels/Domain/Levels/Collection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Liloi\Rune\Modules\Levels\Domain\Levels;
+
+use Liloi\Tools\Collection as AbstractCollection;
+
+/**
+ * @package Engine\Domain\Exercise
+ */
+class Collection extends AbstractCollection
+{
+}
---
+++ Modules/Levels/Domain/Levels/Statuses.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Liloi\Rune\Modules\Levels\Domain\Levels;
+
+class Statuses
+{
+    public const NOT_DEFENDED = 1;
+    public const DEFENDED = 2;
+    public const DEFENDING = 3;
+
+    public static $list = [
+        self::NOT_DEFENDED => 'Not defended level',
+        self::DEFENDING => 'Defending now level',
+        self::DEFENDED => 'Defended level',
+    ];
+}
