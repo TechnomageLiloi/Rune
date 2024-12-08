8ec0306c230bffac2a3190dce1c84cd88afb5970
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-12-08 20:04:58 +0200

    [0.05.038] Add tests to day entity.

--- Modules/Journal/Domain/Road/Test.php
+++ Modules/Journal/Domain/Road/Test.php
@@ -15,4 +15,20 @@ class Test extends TestCase
         $this->assertTrue(file_exists(__DIR__ . '/Entity.php'));
         $this->assertTrue(file_exists(__DIR__ . '/Manager.php'));
     }
+
+    public function testEntity(): void
+    {
+        $data = [
+            'key_day' => random_int(0, 100),
+            'program' => 'test-program',
+            'goal' => 'test-goal'
+        ];
+
+        $entity = Entity::create($data);
+
+        $this->assertEquals($data['key_day'], $entity->getKey());
+        $this->assertEquals($data['program'], $entity->getProgram());
+        $this->assertEquals('<p>' . $data['program'] . '</p>', $entity->getProgramParse());
+        $this->assertEquals($data['goal'], $entity->getGoal());
+    }
 }
