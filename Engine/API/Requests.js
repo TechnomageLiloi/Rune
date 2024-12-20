f2454411d3eb011ce57f7cdc37048f24bfb29a81
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-12-20 13:05:54 +0200

    [0.09.008] Add Rune.Services.loadFromServer method.

--- Engine/API/Requests.js
+++ Engine/API/Requests.js
@@ -1,3 +1,14 @@
 var Rune = {
-
+    Services: {
+        loadFromServer: function loadFile(filePath) {
+            let result = null;
+            let request = new XMLHttpRequest();
+            request.open("GET", filePath, false);
+            request.send();
+            if (request.status == 200) {
+                result = request.responseText;
+            }
+            return result;
+        }
+    }
 };
