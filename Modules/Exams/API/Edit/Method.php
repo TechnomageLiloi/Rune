61ed572110c73beee89955eae5230522333b7f6d
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-12-21 13:01:27 +0200

    [0.09.017] Add Rune.Exams.Opponents.Edit API method.

---
+++ Modules/Exams/API/Edit/Method.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Liloi\Rune\Modules\Exams\API\API;
+
+use Liloi\API\Response;
+use Liloi\Rune\API\Method as SuperMethod;
+use Liloi\Rune\Domain\Databank\Manager as DatabankManager;
+use Liloi\Rune\Modules\Exams\Domain\Opponents\Manager as OpponentsManager;
+
+class Method extends SuperMethod
+{
+    public static function execute(): Response
+    {
+        $URL = $_SERVER['REQUEST_URI'];
+        $RID = DatabankManager::URLtoRID($URL);
+        $entity = OpponentsManager::load(self::getParameter('key_opponent'), $RID);
+
+        $response = new Response();
+        $response->set('render', static::render(__DIR__ . '/Template.tpl', [
+            'entity' => $entity
+        ]));
+
+        return $response;
+    }
+}
---
+++ Modules/Exams/API/Edit/Template.tpl
@@ -0,0 +1,12 @@
+<link href="<?php echo ROOT_URL; ?>/Modules/Exams/API/Edit/Style.css" rel="stylesheet" />
+
+<div id="exams-edit">
+    <table>
+        <tr>
+            <td>Title</td>
+            <td>
+                <input type="text" name="title" value="<?php echo $entity->getTitle(); ?>">
+            </td>
+        </tr>
+    </table>
+</div>
