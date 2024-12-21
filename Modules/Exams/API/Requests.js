98b1f48520b84f917e6b0ed43639120f2c624edc
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-12-21 10:41:06 +0200

    [0.09.015] Add Rune.Exams.Opponents.* JS requests.

---
+++ Modules/Exams/API/Requests.js
@@ -0,0 +1,59 @@
+Rune.Exams.Opponents = {
+    show: function (key_opponent, RID)
+    {
+        API.request('Rune.Exams.Opponents.Show', {
+            'key_opponent': key_opponent,
+            'rid': RID
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    battle: function (key_opponent, RID)
+    {
+        API.request('Rune.Exams.Opponents.Battle', {
+            'key_opponent': key_opponent,
+            'rid': RID
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    edit: function (key_opponent, RID)
+    {
+        API.request('Rune.Exams.Opponents.Edit', {
+            'key_opponent': key_opponent,
+            'rid': RID
+        }, function (data) {
+            $('#page').html(data.render);
+        }, function () {
+
+        });
+    },
+
+    save: function (key_opponent, RID)
+    {
+        if(!confirm('Are you sure?'))
+        {
+            return;
+        }
+
+        const jq_block = $('#blueprint-edit');
+        API.request('Rune.Exams.Opponents.Save', {
+            'key_opponent': key_opponent,
+            'rid': RID,
+            'title': jq_block.find('[name="title"]').val(),
+            'type': jq_block.find('[name="type"]').val(),
+            'program': jq_block.find('[name="program"]').val(),
+            'theory': jq_block.find('[name="theory"]').val()
+        }, function (data) {
+
+        }, function () {
+
+        });
+    }
+}
