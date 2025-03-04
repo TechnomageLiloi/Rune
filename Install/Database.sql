fb2a8069eca1874c9494e32344f25d70f88883b6
Anton Moskalenko <moskalenko.anton@gmail.com>
2025-03-04 19:16:45 +0200

    [0.04.004] Add crystals database table.

--- Install/Database.sql
+++ Install/Database.sql
@@ -21,3 +21,19 @@ CREATE TABLE rune_quests
     foreign key (key_map) references rune_maps(key_map)
         on update cascade on delete cascade
 );
+
+create table rune_crystals
+(
+    key_crystal timestamp not null,
+    key_quest varchar(250) not null,
+    key_map varchar(666) not null,
+    data json not null,
+    constraint rune_crystals_pk
+        primary key (key_crystal),
+    constraint rune_crystals_rune_maps_rid_fk
+        foreign key (key_map) references rune_maps (key_map)
+            on update cascade on delete cascade,
+    constraint rune_crystals_rune_quests_key_quest_fk
+        foreign key (key_quest, key_map) references rune_quests (key_quest, key_map)
+            on update cascade on delete cascade
+);
